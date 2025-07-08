7634340c9ece220becaf776a08cebc2a4fdfa4f5
Deepak singh <deepakniu09@gmail.com>
2025-07-08 12:21:59 +0530

    first commit

--- train_model.py
+++ train_model.py
@@ -14,6 +14,7 @@ from sklearn.inspection import permutation_importance
 import shap
 import joblib
 import warnings
+from job_grouper import JobGrouper
 warnings.filterwarnings('ignore')
 
 # Constants
@@ -31,36 +32,7 @@ class SalaryCorrector(BaseEstimator, TransformerMixin):
         if 'Salary' in X.columns:
             X['Salary'] = X['Salary'].apply(lambda x: x if x > THRESHOLD_SALARY else x * 100)
         return X
-
-class JobGrouper(BaseEstimator, TransformerMixin):
-    """Groups similar job titles into categories"""
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        X = X.copy()
-
-        def group_job_titles(title):
-            if pd.isna(title):
-                return "Other"
-            title = str(title).lower()
-            if any(word in title for word in ['manager', 'director', 'vp', 'chief', 'executive']):
-                return "Leadership"
-            elif any(word in title for word in ['engineer', 'developer', 'scientist', 'technical']):
-                return "Technical"
-            elif any(word in title for word in ['analyst', 'research', 'data']):
-                return "Analyst"
-            elif any(word in title for word in ['sales', 'account', 'business development']):
-                return "Sales"
-            elif any(word in title for word in ['marketing', 'brand', 'product']):
-                return "Marketing"
-            elif any(word in title for word in ['hr', 'human resource', 'recruiter']):
-                return "HR"
-            else:
-                return "Other"
-
-        X['Job_Group'] = X['Job Title'].apply(group_job_titles)
-        return X
+    
 
 class FeatureEngineer(BaseEstimator, TransformerMixin):
     """Creates new features from existing ones"""
